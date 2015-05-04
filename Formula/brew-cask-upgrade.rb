d6402a8382597c9896f23f02ab16dd9f900fc1c1
Jonathan Chan <ycjc@outlook.com>
2015-05-04 18:07:21 +0800

    Update formula

--- Formula/brew-cask-upgrade.rb
+++ Formula/brew-cask-upgrade.rb
@@ -1,11 +1,10 @@
 class BrewCaskUpgrade < Formula
   homepage "https://github.com/NathanJang/homebrew-cask-upgrade/"
-  version "1.0"
-  url "git://github.com/NathanJang/homebrew-cask-upgrade.git"
+  url "https://github.com/NathanJang/homebrew-cask-upgrade.git"
 
   def install
-    FileUtils.mv "brew-cask-upgrade.sh", "brew-cask-upgrade"
-    FileUtils.mv "brew-cask-clean-old.sh", "brew-cask-clean-old"
+    mv "brew-cask-upgrade.sh", "brew-cask-upgrade"
+    mv "brew-cask-clean-old.sh", "brew-cask-clean-old"
     bin.install "brew-cask-upgrade"
     bin.install "brew-cask-clean-old"
     (bin + "brew-cask-upgrade").chmod 0755
